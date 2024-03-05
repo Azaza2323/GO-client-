b7a38b8270c8832bb43ea8ac74ac04214c6ce5fc
azamattt <sabdenbek.4@gmail.com>
2024-03-05 18:47:15 +0500

    Committing changes to profilePage/index.jsx before branch switch

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -1,8 +1,5 @@
-import { Box } from "@mui/material";
-import { useSelector } from "react-redux";
 import Navbar from "../navbar";
 const ProfilePage= () => {
-    // const { _id, picturePath } = useSelector((state) => state.user);
 
     return (
         <div>
